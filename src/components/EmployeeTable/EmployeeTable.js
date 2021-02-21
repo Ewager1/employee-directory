f5aedf301bd235a49df0b45624bc4531fb2cad37
Ethan <69365623+Ewager1@users.noreply.github.com>
2021-02-21 13:29:46 -0500

    submit button now successfully filters table by last name input

--- src/components/EmployeeTable/EmployeeTable.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-function EmployeeTable(props) {
-  console.log("test", props);
-  return <div>{props.employeeInfo[0].gender}</div>;
-}
-
-export default EmployeeTable;
